15d45c2198a2dbe6aeb6319e4bf5f052bc1dc978
jbactasa <107353049+jbactasa@users.noreply.github.com>
2022-06-22 15:20:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const myURL = new URL(window.location.href);
+     console.log(myURL.searchParams.get("name"));
+
+
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
+const newYears = "1 Jan 2023";
+  
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+  
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+    // '0${time} doesn't work so think of a way to fix this
+   // daysEl.innerHTML = days;
+    //hoursEl.innerHTML = formatTime(hours);
+    //minsEl.innerHTML = formatTime(mins);
+    //secondsEl.innerHTML = formatTime(seconds);
+  }
+//function formatTime(time) {
+  //return time < 10 ? '0${time}' : time;
+//}
+  
+countdown();
+
+setInterval(countdown, 1000);
+
